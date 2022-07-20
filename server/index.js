1aa5287d36bdbbc63c7f6670bd98174810897899
emma <yinhongduan@gmail.com>
2022-07-20 15:09:06 -0700

    126

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../index.html"));
 });
 app.get("/css", (req, res) => {
-  res.sendFile(path.join(__dirname, "../styles.css"));
+  res.sendFile(path.join(__dirname, "../style.css"));
 });
 app.get("/js", (req, res) => {
   res.sendFile(path.join(__dirname, "../main.js"));
