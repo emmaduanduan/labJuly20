f843e4cc09ad4e8432ef0dfad4c89990b4af94c5
emma <yinhongduan@gmail.com>
2022-07-21 22:48:04 -0700

    hehe

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: "fc7ee2aeb631406aa32b2b725442abdc",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+try {
+  nonExistentFunction();
+} catch (error) {
+  console.error("This is a mistake.");
+}
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../index.html"));
 });
